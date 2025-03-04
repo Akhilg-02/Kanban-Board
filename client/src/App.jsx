c68ce1eadbbd37ee0b220f5f93cd541ae364160a
unknown <guptaakhil0206@gmail.com>
2025-03-04 18:28:03 +0530

    modified App file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,12 @@
 import { ApiProvider } from "./Context/ApiContext";
-import { DragDropProvider } from "./Context/DndContext";
 import AppRoutes from "./Routes/AppRoutes";
-import { HTML5Backend } from "react-dnd-html5-backend";
 
 function App() {
   return (
     <div>
-      <DragDropProvider backend={HTML5Backend}>
-        <ApiProvider>
+      <ApiProvider>
           <AppRoutes />
-        </ApiProvider>
-      </DragDropProvider>
+      </ApiProvider>
     </div>
   );
 }
